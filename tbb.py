416574dc5816882731d7370dbe2396e98c2e7166
Kris Barrantes <kris.barrantes@outlook.com>
2021-09-30 15:49:33 -0500

    Use Integrated Scheduler

--- tbb.py
+++ tbb.py
@@ -74,6 +74,30 @@ def addtoblocklist(id):
     conn.commit()
     return 0
 
+def checkfollowersforbots(self, c, e):
+    userinformation = twitch.get_users(logins=chan)
+    pag = ""
+    ctr = 0
+    logging.warning('Checking followers for bots and banning')
+    while True:
+        if len(pag) > 0:
+            foll = twitch.get_users_follows(first=100, to_id=userinformation['data'][0]['id'], after=pag)
+        else:
+            foll = twitch.get_users_follows(first=100, to_id=userinformation['data'][0]['id'])
+        for a in range(len(foll['data'])):
+            ctr += 1
+            badactor = foll['data'][a]['from_login']
+            if isuserloginindb(badactor):
+                c.privmsg(self.channel, '/ban ' + badactor + 'This Username has been identified in the CommanderRoot Blocklist. If you feel this is in error please contact CommanderRoot on Twitter to have your ID removed from the list. Once we update our copy of the list your account will be unbanned if it has been removed.')
+                addtoblocklist(badactor)
+                logging.warning(badactor + ' is Following ' + chan + ' AND has been banned')
+        if len(foll['pagination']) > 0:
+            pag = foll['pagination']['cursor']
+        else:
+            break
+    print('Processed ' + str(ctr) + ' followers for ' + chan)
+    return 0
+
 class TwitchBot(irc.bot.SingleServerIRCBot):
     def __init__(self, username, client_id, token, channel):
         self.client_id = client_id
@@ -148,7 +172,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         c.cap('REQ', ':twitch.tv/commands')
         c.join(self.channel)
         sleep(10)
-        self.reactor.scheduler.execute_every('60', self.checkfollowersforbots(c, e))
+        self.reactor.scheduler.execute_every(60, self.checkfollowersforbots(self, c, e))
 
     def on_join(self, c, e):
         usrid = e.source.split('!')[0]
