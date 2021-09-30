0e8cd28565e9ba1740717fe8cd347243da76605a
Kris Barrantes <kris.barrantes@outlook.com>
2021-09-30 14:10:22 -0500

    Use Integrated Scheduler

--- tbb.py
+++ tbb.py
@@ -148,7 +148,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         c.cap('REQ', ':twitch.tv/commands')
         c.join(self.channel)
         #irc.bot.SingleServerIRCBot.reactor_class.scheduler_class.execute_every(self, period=60, func=self.checkfollowersforbots(c, e))
-        irc.schedule.DefaultScheduler.add(self, tempora.schedule.PeriodicCommand(60, func=self.checkfollowersforbots(c, e)))
+        irc.schedule.DefaultScheduler.add(self, tempora.schedule.PeriodicCommand.after(60, func=self.checkfollowersforbots(c, e)))
 
     def on_join(self, c, e):
         usrid = e.source.split('!')[0]
